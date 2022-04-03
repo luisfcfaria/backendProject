43800d071cbd2011176e00f9a21cc8b0ea3a782f
Tiago Correia <correiatiago1987@gmail.com>
2022-04-04 00:28:31 +0100

    heroku

--- src/main/java/academy/mindswap/flight/gatewayemail/EmailTemplate.java
+++ src/main/java/academy/mindswap/flight/gatewayemail/EmailTemplate.java
@@ -1,9 +1,9 @@
 package academy.mindswap.flight.gatewayemail;
 
 public enum EmailTemplate {
-    WELCOME_EMAIL(  "d-37128938d12b4b2786d85bb3d0434d38"),
-    BOOKED_FLIGHT("d-65c70b9a13fb4931a8e484deb7a394ef"),
-    CANCELED_FLIGHT("d-5ec213b741d748d19d25d2d1f1abac91");
+    WELCOME_EMAIL("d-8a8ebe80cbaa40369ac982780c167438"),
+    BOOKED_FLIGHT("d-cc16ccb8279442b1a54e1c2faad5ff34"),
+    CANCELED_FLIGHT("d-9c901669865246409789a3957187bba0");
 
     private String name;
 
